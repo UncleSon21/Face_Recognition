90fa50907c01b7959835c47e0cdcdd1421d773d0
khanhsaan <anthonykhanhan@gmail.com>
2024-09-25 13:02:19 +1000

    try to push

---
+++ cv_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:12ebf1193a627e0466418d7e55830b579889d19b0422abcefbbf99f4709cf3a5
+size 4299
--- data_preprocessing.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2cf9d8fbff74081754216d8b65fd90221b4514b3f86fea92a0de6e7ae9c6875d
-size 4295
