a9cfa9edfe6eb403b5ab567e463e22861c0b53e1
khanhsaan <anthonykhanhan@gmail.com>
2024-09-25 12:15:01 +1000

    Finished testing cv model

---
+++ data_preprocessing.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:52d14dd645c26e5223d7e41d965b7da6cc6f094de128e4adf7f4009917b53f5e
+size 4294
---
+++ train_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:211cf153cac0c3c6390c2b100d499a6c8da311374d50b898662f3ba86d98d952
+size 2532
